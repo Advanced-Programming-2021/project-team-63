3c91cc4c5f5856c08da7a5650785890a84fd60ad
MGH <mahdi13812002@gmail.com>
2021-05-12 13:21:36 +0430

    designing d:

--- src/main/java/Controller/ProgramController.java
+++ src/main/java/Controller/ProgramController.java
@@ -8,20 +8,16 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.*;
-import java.util.regex.Matcher;
 import java.util.stream.Collectors;
 
 public class ProgramController {
-    private final String usersInfoPath = "src\\main\\java\\Database\\UserInfo.txt";
-    private final String monstersInfoPath = "src\\main\\java\\Database\\MonstersInfo.txt";
-    private final String spellAndTrapsINfoPath = "src\\main\\java\\Database\\Spell&TrapsInfo.txt";
+    private final static String usersInfoPath = "src\\main\\java\\Database\\UserInfo.txt";
+    private final static String monstersInfoPath = "src\\main\\java\\Database\\MonstersInfo.txt";
+    private final static String spellAndTrapsINfoPath = "src\\main\\java\\Database\\Spell&TrapsInfo.txt";
     private AccountJson loggedInUser ;
 
-    // ToDo: get corrct value for every fucntion from matcher
-    public ApiMessage register(Matcher matcher) throws Exception {
-        String password = "";
-        String nickname = "";
-        String username = "";
+
+    public ApiMessage register(String username, String password, String nickname) throws Exception {
 
         if(doesUserWithThisUsernameExist(username)){
             return new ApiMessage(ApiMessage.error,"user with username " + username + " already exists");
@@ -38,9 +34,7 @@ public class ProgramController {
 
     }
 
-    public ApiMessage login(Matcher matcher) throws Exception {
-        String username = "";
-        String password = "";
+    public ApiMessage login(String username, String password) throws Exception {
 
         if(!doesUserWithThisUsernameExist(username)){
             return new ApiMessage(ApiMessage.error,"Username and password didn’t match!");
@@ -57,19 +51,19 @@ public class ProgramController {
 
     }
 
-    public ApiMessage logout(Matcher matcher) throws Exception {
+    public ApiMessage logout() throws Exception {
         setLoggedInUser(null);
         return new ApiMessage(ApiMessage.successful,"user logged out successfully!");
     }
 
-    public ApiMessage showScoreboard(Matcher matcher) throws Exception {
+    public ApiMessage showScoreboard() throws Exception {
         var users = getUsersInfo();
         ArrayList<ScoreboardInfo> scoreboard = (ArrayList<ScoreboardInfo>) users.stream().map(x -> new ScoreboardInfo(x.getNickname(),x.getScore())).collect(Collectors.toList());
         return new ApiMessage(ApiMessage.successful,new Gson().toJson(scoreboard));
     }
 
-    public ApiMessage changeNickname(Matcher matcher) throws Exception {
-        String nickname = "" ;
+    public ApiMessage changeNickname(String nickname) throws Exception {
+
         if(doesUserWithThisNicknameExist(nickname)){
             return new ApiMessage(ApiMessage.error,"user with nickname " + nickname + " already exists");
         }
@@ -80,9 +74,7 @@ public class ProgramController {
         }
     }
 
-    public ApiMessage changePassword(Matcher matcher) throws Exception {
-        String currentPassword = "" ;
-        String newPassword = "";
+    public ApiMessage changePassword(String currentPassword, String newPassword) throws Exception {
 
         if(!loggedInUser.getPassword().equals(currentPassword)){
             return new ApiMessage(ApiMessage.error,"current password is invalid");
@@ -99,8 +91,7 @@ public class ProgramController {
         }
     }
 
-    public ApiMessage createDeck(Matcher matcher) throws Exception {
-        String deckName = "";
+    public ApiMessage createDeck(String deckName) throws Exception {
 
         if(loggedInUser.getDeckByName(deckName) == null){
             return new ApiMessage(ApiMessage.error,"deck with name "+deckName+"  already exists");
@@ -113,9 +104,7 @@ public class ProgramController {
         }
     }
 
-    public ApiMessage deleteDeck(Matcher matcher) throws Exception {
-        String deckName = "";
-
+    public ApiMessage deleteDeck(String deckName) throws Exception {
         var deck = loggedInUser.getDeckByName(deckName);
 
         if(deck == null){
@@ -129,8 +118,7 @@ public class ProgramController {
         }
     }
 
-    public ApiMessage selectActiveDeck(Matcher matcher) throws Exception {
-        String deckName = "";
+    public ApiMessage selectActiveDeck(String deckName) throws Exception {
         var deck = loggedInUser.getDeckByName(deckName);
 
         if(deck == null){
@@ -144,10 +132,7 @@ public class ProgramController {
         }
     }
 
-    public ApiMessage addCardToDeck(Matcher matcher) throws Exception {
-        String cardName = "";
-        String deckName = "";
-        boolean inSideDeck = false;
+    public ApiMessage addCardToDeck(String cardName, String deckName, boolean inSideDeck) throws Exception {
         var deck = loggedInUser.getDeckByName(deckName);
 
         if(!loggedInUser.doesHaveThisCard(cardName)){
@@ -180,12 +165,9 @@ public class ProgramController {
         }
     }
 
-    public ApiMessage removeCardFromDeck(Matcher matcher) throws Exception {
+    public ApiMessage removeCardFromDeck(String cardName, String deckName, boolean fromSideDeck) throws Exception {
         //hame ro hazf konim ya yekisho
         //manaye mojoodi chiye
-        String cardName = "";
-        String deckName = "";
-        boolean fromSideDeck = false;
         var deck = loggedInUser.getDeckByName(deckName);
 
         if(deck == null){
@@ -211,15 +193,13 @@ public class ProgramController {
 
     }
 
-    public ApiMessage showAllDeck(Matcher matcher) throws Exception {
+    public ApiMessage showAllDeck() throws Exception {
         //how fix Gson null values
         var ans = new ShowAllDecksJson(loggedInUser);
         return new ApiMessage(ApiMessage.successful,new Gson().toJson(ans));
     }
 
-    public ApiMessage showDeck(Matcher matcher) throws Exception {
-        String deckName = "";
-        boolean isSideDeck = false;
+    public ApiMessage showDeck(String deckName, boolean isSideDeck) throws Exception {
         var deck = loggedInUser.getDeckByName(deckName);
 
         if(deck == null){
@@ -237,36 +217,38 @@ public class ProgramController {
         }
     }
 
-
     public ApiMessage showPurchasedCards() throws Exception {
         ArrayList<CardGeneralInfo> purchaseCards =  new ArrayList<>();
         for (CardJson card : loggedInUser.getPurchasedCards()) {
-            if(isMonsterExistWithThisName(card.getName())){
+            if(doesMonsterExistWithThisName(card.getName())){
                 MonsterJson monster = getMonsterByName(card.getName());
+                assert monster != null;
                 purchaseCards.add(new CardGeneralInfo(monster));
             }
             else{
                 SpellAndTrapJson spellAndTrap = getSpellAndTrapByName(card.getName());
+                assert spellAndTrap != null;
                 purchaseCards.add(new CardGeneralInfo(spellAndTrap));
             }
         }
         return new ApiMessage(ApiMessage.successful,new Gson().toJson(purchaseCards));
     }
 
-    public ApiMessage buyCard(Matcher matcher) throws Exception {
-        String cardName = "";
+    public ApiMessage buyCard(String cardName) throws Exception {
 
-        if(!isMonsterExistWithThisName(cardName)&&!isSpellOrTrapExsistWithThisName(cardName)){
+        if(!doesMonsterExistWithThisName(cardName)&&!doesSpellOrTrapExistsWithThisName(cardName)){
             return new ApiMessage(ApiMessage.error,"there is no card with this name");
         }
 
         CardGeneralInfo card;
-        if(isMonsterExistWithThisName(cardName)){
+        if(doesMonsterExistWithThisName(cardName)){
             MonsterJson monster = getMonsterByName(cardName);
+            assert monster != null;
             card = new CardGeneralInfo(monster);
         }
         else{
             SpellAndTrapJson spellAndTrap = getSpellAndTrapByName(cardName);
+            assert spellAndTrap != null;
             card = new CardGeneralInfo(spellAndTrap);
         }
 
@@ -280,7 +262,7 @@ public class ProgramController {
         return new ApiMessage(ApiMessage.successful,"card successfully purchased");
     }
 
-    public ApiMessage showShopCards(Matcher matcher) throws Exception {
+    public ApiMessage showShopCards() throws Exception {
         ArrayList<CardGeneralInfo> ans = new ArrayList<>();
 
         for (MonsterJson monster : getMonstersInfo()) {
@@ -296,18 +278,62 @@ public class ProgramController {
 
     private void getCardGeneralInfoForShowDeck(ShowDeckJson ans, ArrayList<CardJson> deck) throws IOException {
         for (CardJson card : deck) {
-            if(isMonsterExistWithThisName(card.getName())){
+            if(doesMonsterExistWithThisName(card.getName())){
                 MonsterJson monster = getMonsterByName(card.getName());
+                assert monster != null;
                 ans.addToMonsters(new CardGeneralInfo(monster));
             }
             else{
                 SpellAndTrapJson spellAndTrap = getSpellAndTrapByName(card.getName());
+                assert spellAndTrap != null;
                 ans.addToSpellAndTraps(new CardGeneralInfo(spellAndTrap));
             }
         }
     }
 
-    private boolean isMonsterExistWithThisName(String name) throws IOException {
+    private void changeUserInfoInDataBase(AccountJson newUserInfo) throws IOException {
+        ArrayList<AccountJson> users = getUsersInfo();
+        FileWriter fileWriter = new FileWriter(usersInfoPath);
+        for(int i = 0 ; i < users.size() ; i++){
+            if(users.get(i).getUsername().equals(newUserInfo.getUsername())){
+                users.remove(i);
+                users.add(newUserInfo);
+                break;
+            }
+        }
+        fileWriter.write(new Gson().toJson(users));
+        fileWriter.close();
+    }
+
+    private void addUserInfoToDatabase(String username, String password, String nickname) throws IOException {
+        AccountJson newUser = new AccountJson(username, password, nickname);
+        ArrayList<AccountJson> users = getUsersInfo();
+        FileWriter fileWriter = new FileWriter(usersInfoPath);
+
+        users.add(newUser);
+        fileWriter.write(new Gson().toJson(users));
+        fileWriter.close();
+    }
+
+    public boolean doesUserWithThisUsernameExist(String username) throws IOException {
+        ArrayList<AccountJson> users = getUsersInfo();
+        for (AccountJson user : users) {
+            if(user.getUsername().equals(username))
+                return true;
+        }
+        return false;
+    }
+
+    public boolean doesUserWithThisNicknameExist(String nickname) throws IOException {
+        ArrayList<AccountJson> users = getUsersInfo();
+        for (AccountJson user : users) {
+            if(user.getNickname().equals(nickname))
+                return true;
+        }
+        return false;
+    }
+
+    private boolean doesMonsterExistWithThisName(String name) throws IOException {
         var monsters = getMonstersInfo();
         for (MonsterJson monster : monsters) {
             if(monster.getName().equals(name))
@@ -316,13 +342,19 @@ public class ProgramController {
         return false ;
     }
 
-    private boolean isSpellOrTrapExsistWithThisName(String name) throws IOException {
+    private boolean doesSpellOrTrapExistsWithThisName(String name) throws IOException {
         var spellAndTraps = getSpellAndTrapsInfo();
         for (SpellAndTrapJson spellAndTrap : spellAndTraps) {
             if(spellAndTrap.getName().equals(name))
                 return true;
         }
         return false ;
+    }
+
+    private boolean isCorrectPassword(String username, String password) throws IOException {
+        AccountJson user = getUserInfoByUsername(username);
+        assert user != null;
+        return user.getPassword().equals(password);
     }
 
     private MonsterJson getMonsterByName(String name) throws IOException {
@@ -343,6 +375,15 @@ public class ProgramController {
         return null;
     }
 
+    private AccountJson getUserInfoByUsername(String username) throws IOException {
+        ArrayList<AccountJson> users = getUsersInfo();
+        for (AccountJson user : users) {
+            if(user.getUsername().equals(username))
+                return user;
+        }
+        return null;
+    }
+
     private ArrayList<MonsterJson> getMonstersInfo() throws IOException {
         String json = new String(Files.readAllBytes(Paths.get(monstersInfoPath)));
         return new Gson().fromJson(json ,new TypeToken<List<MonsterJson>>(){}.getType());
@@ -353,61 +394,6 @@ public class ProgramController {
         return new Gson().fromJson(json ,new TypeToken<List<SpellAndTrapJson>>(){}.getType());
     }
 
-    private void changeUserInfoInDataBase(AccountJson newUserInfo) throws IOException {
-        ArrayList<AccountJson> users = getUsersInfo();
-        FileWriter fileWriter = new FileWriter(usersInfoPath);
-        for (AccountJson user : users) {
-            if(user.getUsername().equals(newUserInfo.getUsername())){
-                user = newUserInfo;
-                break;
-            }
-        }
-        fileWriter.write(new Gson().toJson(users));
-        fileWriter.close();
-    }
-
-    public boolean doesUserWithThisUsernameExist(String username) throws IOException {
-       ArrayList<AccountJson> users = getUsersInfo();
-        for (AccountJson user : users) {
-            if(user.getUsername().equals(username))
-                return true;
-        }
-        return false;
-    }
-
-    public boolean doesUserWithThisNicknameExist(String nickname) throws IOException {
-        ArrayList<AccountJson> users = getUsersInfo();
-        for (AccountJson user : users) {
-            if(user.getNickname().equals(nickname))
-                return true;
-        }
-        return false;
-    }
-
-    private void addUserInfoToDatabase(String username, String password, String nickname) throws IOException {
-        AccountJson newUser = new AccountJson(username, password, nickname);
-        ArrayList<AccountJson> users = getUsersInfo();
-        FileWriter fileWriter = new FileWriter(usersInfoPath);
-
-        users.add(newUser);
-        fileWriter.write(new Gson().toJson(users));
-        fileWriter.close();
-    }
-
-    private boolean isCorrectPassword(String username, String password) throws IOException {
-        AccountJson user = getUserInfoByUsername(username);
-        return user.getPassword().equals(password);
-    }
-
-    private AccountJson getUserInfoByUsername(String username) throws IOException {
-        ArrayList<AccountJson> users = getUsersInfo();
-        for (AccountJson user : users) {
-            if(user.getUsername().equals(username))
-                return user;
-        }
-        return null;
-    }
-
     private ArrayList<AccountJson> getUsersInfo() throws IOException {
         String json = new String(Files.readAllBytes(Paths.get(usersInfoPath)));
         return new Gson().fromJson(json ,new TypeToken<List<AccountJson>>(){}.getType());
@@ -416,7 +402,4 @@ public class ProgramController {
     private void setLoggedInUser(AccountJson loggedInUser){
         this.loggedInUser = loggedInUser;
     }
-
-
-
 }
