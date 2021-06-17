ee68a14604ad88c675ea62398efe3e1b3487046f
KhosroMoeini <khosromoeini1381@gmail.com>
2021-06-17 17:43:22 +0430

    Khosro

--- src/main/java/Model/Game/Player.java
+++ src/main/java/Model/Game/Player.java
@@ -1,5 +1,6 @@
 package Model.Game;
 
+import java.io.*;
 import java.util.*;
 import Model.*;
 import Model.Game.Card.*;
@@ -11,7 +12,8 @@ public class Player {
     private String nickname;
     private Field field;
     private int lp;
-    private Deck deck;
+    private ArrayList<Card> mainDeck;
+    private ArrayList<Card> sideDeck;
     private ArrayList<Card> cards;
     private ArrayList<Card> hand;
 
@@ -23,7 +25,8 @@ public class Player {
         setNickname(account.getNickname());
         setField(new Field());
         setLp(8000);
-        setDeck(account.getActiveDeck());
+        setMainDeck(account.getactiveDeck().getMainDeck());
+        setSideDeck(account.getactiveDeck().getSideDeck());
         setHand(new ArrayList<Card>());
         setSelectedCard(null);
     }
@@ -57,12 +60,52 @@ public class Player {
         this.lp -= amount;
     }
 
-    public void setDeck(Deck deck) {
-        this.deck = deck;
+    public void setMainDeck(ArrayList<String> mainDeckNames) {
+        boolean isMonster = false;
+        String line = ""; 
+        for(String name : mainDeckNames){ 
+            try{  
+                //parsing a CSV file into BufferedReader class constructor  
+                BufferedReader reader = new BufferedReader(new FileReader("\\src\\main\\java\\Database\\Monster.csv"));  
+                while ((line = reader.readLine()) != null){  
+                    String[] card = line.split(",");
+                    if(card[0].equals(name)){
+                        isMonster = true;
+                        if(card[4].equals("Normal")) mainDeck.add(new MonsterCard());
+                        else if(card[4].equals("Ritual")) mainDeck.add(new RitualMonsterCard());
+                        else mainDeck.add(new EffectiveMonsterCard());
+                    }
+                }
+                reader.close(); 
+            }catch (IOException e){  
+            }
+            if(!isMonster){ 
+                try{  
+                    //parsing a CSV file into BufferedReader class constructor  
+                    BufferedReader reader = new BufferedReader(new FileReader("\\src\\main\\java\\Database\\SpellTrap.csv"));  
+                    while ((line = reader.readLine()) != null){  
+                        String[] card = line.split(",");
+                        if(card[0].equals(name)){
+                            mainDeck.add(new SpellCard());
+                        }
+                    }
+                    reader.close(); 
+                }catch (IOException e){  
+                }
+            }
+        }
     }
 
-    public Deck getDeck() {
-        return deck;
+    public void setSideDeck(ArrayList<String> sideDeckNames){
+
+    }
+
+    public ArrayList<Card> getMainDeck() {
+        return mainDeck;
+    }
+
+    public ArrayList<Card> getSideDeck() {
+        return sideDeck;
     }
 
     public void setCards(ArrayList<Card> cards) {
